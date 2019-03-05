d53f9f4c11aecacf2f80717041e22a8dacaad921
John <johnbaironlr@gmail.com>
2019-03-04 22:36:53 -0500

    nuevo

--- hola_mundo.rb
+++ hola_mundo.rb
@@ -2,6 +2,46 @@
 puts "Hola Mundo"
 puts "segunda linea"
 =end
-
+#----------------------------------------------------
 # puts "Hola Mundo"
 # puts "segunda linea"
+#----------------------------------------------------
+# puts "Hola Mundo"
+# puts "segunda linea"
+# puts "Hola desde \"mi\" cpu"
+#----------------------------------------------------
+# print "Escribe una frase: "
+# frase = gets.chomp
+#
+# puts frase
+#----------------------------------------------------
+# print "Escribe tu nombre: "
+# nombre = gets.chomp
+#
+# puts "Hola #{nombre}!"
+#----------------------------------------------------
+# print "Ingresar primer número: "
+# num1 = gets.chomp.to_i
+#
+# print "Ingresar segundo número: "
+# num2 = gets.chomp.to_i
+#
+# puts "El resultado es #{num1 + num2}"
+#----------------------------------------------------
+
+t = Time.now
+# puts t.strftime("%Y")
+
+print "En que año naciste?: "
+año = gets.chomp.to_i
+edad = t.strftime("%Y").to_i - año
+
+if edad == 1
+  puts "Tienes un año"
+elsif edad > 120
+  puts "Imposible nadie tiene esa edad"
+elsif edad < 0
+  puts "Imposible aun no has nacido"
+else
+  puts "Tienes #{edad} años"
+end
---
+++ perimetro.rb
@@ -0,0 +1,7 @@
+# lado = 5
+
+print "Ingresar el lado del cuadrado: "
+lado = gets.chomp.to_i
+
+puts "El perimetro del cuadrado de lado 5 es: #{lado*4}"
+puts "El area del cuadrado de lado 5 es: #{lado**2}"
